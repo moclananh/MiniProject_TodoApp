ad47fd56dcc1934cdaf3cda0240ba35018504aa8
Thanh Check Merg <kristino5223@gmail.com>
2024-12-13 16:03:39 +0700

    init commit

---
+++ src/2.Infrastructure/TodoApp.Infrastructure/Dtos/TodoDtos/UpdateTodoRequest.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace TodoApp.Infrastructure.Dtos.TodoDtos
+{
+    public class UpdateTodoRequest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TaskStatus Status { get; set; }
+        public int Priority { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool Star { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
